9919f8efe2155bc33f0beb0e46613834dbb03d3c
Nehal Reda <nehalreda14@gmail.com>
2025-08-22 12:40:49 +0300

    newNames

---
+++ Day6/js/main.js
@@ -0,0 +1,7 @@
+let btn = document.getElementById('btn');
+
+function description() {
+    alert(`A T-shirt is a casual piece of clothing, usually made of cotton, with short
+                            sleeves and no collar. It’s comfortable, lightweight, and perfect for everyday wear`);
+}
+// btn.addEventListener('click', description);
