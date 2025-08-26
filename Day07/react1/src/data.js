e919c2052e7bb9ad18d42e90b1c9a9da21e8c94e
Nehal Reda <nehalreda14@gmail.com>
2025-08-26 20:39:39 +0300

    new

---
+++ Day07/react1/src/data.js
@@ -0,0 +1,36 @@
+import img from '../src/assets/2010.i123.005..car headlights AD composition.jpg';
+
+const data = [
+    {
+        id: 1,
+        title: 'Car Maintenance',
+        image: img,
+        description: 'Best car maintenance service'
+    },
+    {
+        id: 2,
+        title: 'Oil Change',
+        image: img,
+        description: 'High-quality oil change'
+    },
+    {
+        id: 3,
+        title: 'Tire Replacement',
+        image: img,
+        description: 'Affordable tire replacement'
+    },
+    {
+        id: 3,
+        title: 'Tire Replacement',
+        image: img,
+        description: 'Affordable tire replacement'
+    },
+    {
+        id: 3,
+        title: 'Tire Replacement',
+        image: img,
+        description: 'Affordable tire replacement'
+    }
+];
+
+export default data;
