3842c96e99966ee1390a5903f1abc619db0b3456
Nehal Reda <nehalreda14@gmail.com>
2025-08-30 00:22:09 +0300

    new

--- Day6/js/main.js
+++ Day6/js/main.js
@@ -1,7 +1,40 @@
-let btn = document.getElementById('btn');
 
-function description() {
-    alert(`A T-shirt is a casual piece of clothing, usually made of cotton, with short
-                            sleeves and no collar. It’s comfortable, lightweight, and perfect for everyday wear`);
+function des() {
+    alert("Exploring the trends and technologies shaping the next decade of the web.");
 }
-// btn.addEventListener('click', description);
+
+let container = document.getElementById('container');
+
+const fetchData = () => {
+    fetch("https://test-api-v1-vert.vercel.app/v1/blogs")
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+
+
+            const data = result.slice(0, 4);
+
+
+            container.innerHTML = `
+       
+          ${data
+                    .map(
+                        (item) => `
+                <div class="child">
+                  <h3>${item.title}</h3>
+                  <img src="${item.image}" alt="${item.title}" width="200" />
+                  <p>${item.description}</p>
+                  <button onclick="des()">Add to cart</button>
+                </div>
+              `
+                    )
+                    .join("")}
+        
+      `;
+
+
+        })
+        .catch((error) => console.error("Error fetching data:", error));
+};
+
+fetchData();
